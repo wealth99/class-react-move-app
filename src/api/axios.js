af1b97d95ecbd104300cd42c3269ab688c97dc2b
cheonsu92 <cheonsu92@gmail.com>
2023-02-06 17:33:02 +0900

    axios 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "bfca4380461c3bfee31c9edb1996cdb0",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR"
     }
 });
